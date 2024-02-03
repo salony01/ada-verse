54c05f3d662ca761e87388a4f4fa649a33caf225
vishwa-sadula <sadula.vishwa.cse20@itbhu.ac.in>
2024-02-04 02:57:11 +0530

    epg integration

--- server/server.js
+++ server/server.js
@@ -1,5 +1,3 @@
-
-
 const express = require("express");
 const path = require("path");
 const bcrypt = require("bcrypt");
@@ -54,21 +52,13 @@ app.post("/login",async(req,res)=>{
     res.json({ "CORRECT": "noo" })
   }
 })
-const port = 5000;
-app.listen(5000, () => {
-  console.log("servr started on port 5000");
-});
 
 
-// const express=require('express')
-// const app=express()
 const fetch = require("node-fetch")
 app.get("/api" ,(req,res)=>{
     res.json({"users":["1","2","3"]})
 })
-app.listen(5000,()=>{
-    console.log("server started on port 5000")
-})
+
 
 function sortByKey(array, key) {
     return array.sort(function(a, b) {
@@ -78,49 +68,108 @@ function sortByKey(array, key) {
     });
   }
 
-app.post("/movie", (req,res)=>{
-    
-    let typee= "ott", show="movie", gen="Drama", ratSort=true;
-    let reccApiUrl = "https://cla-recommendation.lgads.tv/recommendation/popular?type="+typee+"&genre="+gen+"&showType="+show+"&limit=10";
-    fetch(reccApiUrl, {
-        method: 'GET',
-        headers : {
-            'Authorization':'Bearer 8BMgmyHiG'
-        }
-    } ).then( res => {
-        return res.json()
-    } )
-    .then(data => {
-        
-        let dataList= data["ott_recommendations"]["contents"];
-        if(ratSort)
-        {
-             dataList= sortByKey(dataList, 'rating')
-        }
-        res.json(dataList);
+
+async function getUrl(dataId)
+{
+  let epgApiUrl = "https://cla-epg.lgads.tv/epg/ott?contentId="+dataId;
+  let contentUrl;
+  const response= await fetch(epgApiUrl, {
+    method: 'GET',
+    headers : {
+        'Authorization':'Bearer 8BMgmyHiG'
     }
-    )
-    .catch(error=>  console.log('Error while fetching from api' ));
+})
 
-} )
+const epgData = await response.json();
+contentUrl= epgData['result'][0]['links'][0]['urls']['web'];
+  return contentUrl;
+}
 
+async function f(dataList, limit)
+{
+  let responseData=[]
+  for(var i=0; i<limit; i++)
+        {
+            let dataId= dataList[i]['id'];
+            let currContent=dataList[i];
+            const contentUrl= await getUrl(dataId);
+            currContent.contentUrl=contentUrl;
+            responseData[i]=currContent;
+        }      
+        return responseData;
 
-app.post("/mov", (req,res)=>{
+}
+
+async function fun2(req)
+{
+  // let typee= "ott", show="movie", gen="Drama",limit=10;
+    let typee= "ott", show=req.body.showType, gen=req.body.genre, limit=10;
+    let reccApiUrl = "https://cla-recommendation.lgads.tv/recommendation/popular?type="+typee+"&genre="+gen+"&showType="+show+"&limit="+limit;
+  const response= await fetch(reccApiUrl, {
+    method: 'GET',
+    headers : {
+        'Authorization':'Bearer 8BMgmyHiG'
+    }
+})
+
+const reccData = await response.json();
+    let dataList= reccData["ott_recommendations"]["contents"];
+        let responseData= await f(dataList,limit);
+            return responseData;
+}
+
+async function fun3(req)
+{
+  // let typee= "ott", show="movie", gen="Drama",limit=10, ratSort=true, yearSort=false;
+    let typee= "ott", show=req.body.showType, gen=req.body.genre, ratSort=req.body.ratSort, yearSort=req.body.yearSort, limit=10;
+    let reccApiUrl = "https://cla-recommendation.lgads.tv/recommendation/popular?type="+typee+"&genre="+gen+"&showType="+show+"&limit="+limit;
+  const response= await fetch(reccApiUrl, {
+    method: 'GET',
+    headers : {
+        'Authorization':'Bearer 8BMgmyHiG'
+    }
+})
+
+const reccData = await response.json();
+    let dataList= reccData["ott_recommendations"]["contents"];
+        let responseData= await f(dataList,limit);
+            // console.log("fin", responseData)
+            //sort
+            if(ratSort)
+            {
+              responseData= sortByKey(responseData, 'rating')
+            }
+            if(yearSort)
+            {
+              responseData= sortByKey(responseData, 'releaseYear')
+            }
+            return responseData;
+}
+
+app.post("/genrePref", async (req,res)=>{
+
+  let responseData=await fun2(req);
+  let returning={}
+        returning.ott_recommendations={};
+        returning.ott_recommendations.contents=responseData;
+    res.json(returning);
     
-    let typee= "ott", show="movie", gen="Drama";
-    let reccApiUrl = "https://cla-recommendation.lgads.tv/recommendation/popular?type="+typee+"&genre="+gen+"&showType="+show+"&limit=1";
-    let dataProcess;
-    fetch(reccApiUrl, {
-        method: 'GET',
-        headers : {
-            'Authorization':'Bearer 8BMgmyHiG'
-        }
-    } ).then( res => {
-        return res.json()
-    } )
-    .then(data=> res.json(data)  )
-    .catch(error=>  console.log('Error while fetching from api' ));
 
 } )
 
+app.post("/genreSort", async (req,res)=>{
+
+  let responseData=await fun3(req);
+  let returning={}
+        returning.ott_recommendations={};
+        returning.ott_recommendations.contents=responseData;
+    res.json(returning);
+    
+
+} )
+
+
+app.listen(5000,()=>{
+    console.log("server started on port 5000")
+})
 
