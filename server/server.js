f8f0096bdd22d89930fd7759f8e37fa0eae76de2
Saloni2004 <garg.22@iitj.ac.in>
2024-02-03 12:39:17 +0530

    added server

---
+++ server/server.js
@@ -0,0 +1,6 @@
+const express=require('express')
+const app=express()
+app.get("/api" ,(req,res)=>{
+    res.json({"users":["1","2","3"]})
+})
+app.listen(5000,()=>{console.log("server started on port 5000")})
